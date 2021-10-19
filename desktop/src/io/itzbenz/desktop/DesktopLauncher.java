73b8abcb4d30126070c1c1e57ffcafc5e54cfaa1
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-10-19 06:11:20 +0200

    Init

---
+++ desktop/src/io/itzbenz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.itzbenz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.itzbenz.GdxTestingItz;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxTestingItz(), config);
+	}
+}
